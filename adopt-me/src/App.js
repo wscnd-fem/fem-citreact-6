2d14aae6326debb8355d5a79ac2e3bab42698366
wscnd <wellingson@gmail.com>
2021-04-08 10:44:08 -0300

    chore: core concepts - hooks

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,36 +1,15 @@
 import ReactDOM from 'react-dom';
-import Pet from './Pet';
-
-// const App = () => {
-//   return React.createElement('div', {}, [
-//     React.createElement('h1', {}, 'Adopt Me!'),
-//     React.createElement(Pet, {
-//       name: 'Luna',
-//       animal: 'Dog',
-//       breed: 'Havanese',
-//     }),
-
-//     React.createElement(Pet, {
-//       name: 'Pepper',
-//       animal: 'Bird',
-//       breed: 'Cockatiel',
-//     }),
-
-//     React.createElement(Pet, {
-//       name: 'Sudo',
-//       animal: 'Dog',
-//       breed: 'Wheaten Terrier',
-//     }),
-//   ]);
-// };
+// import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <SearchParams />
+      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
       <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" /> */}
     </div>
   );
 };
---
+++ adopt-me/src/SearchParams.js
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+
+const ANIMALS = ['Bird', 'Cat', 'Dog', 'Rabbit', 'Reptile'];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState('Seattle, WA');
+  const [animal, setAnimal] = useState('');
+  const [breed, setBreed] = useState('');
+  const breeds = [];
+
+  return (
+    <div className="search-params">
+      <form action="">
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder="Location"
+          />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option value="" />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option value="" />
+            {breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
